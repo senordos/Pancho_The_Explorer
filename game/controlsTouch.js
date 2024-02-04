1a27a3c3d3b86b67a9d41899ad27dbb4f07a0add
senordos <christian@fishpod.co.uk>
2024-02-04 12:00:01 +0000

    Updates

--- game/controlsTouch.js
+++ game/controlsTouch.js
@@ -30,14 +30,18 @@ function onTouchStart(event)
 					    //if (x>(900 * canvasScale + margin) &&  x<(1020 * canvasScale + margin)) { touchButtons.resetlevel.pressed = true; touchButtons.resetlevel.touchId = id }
 						//player1_ResetLevelPressed = true;
 						player1_ScreenTouched = true;
+						sound.playSound(SND_CHILLI);
+
 
 				}
 				else if (gameState == "LEVEL_COMPLETE_WAIT_FOR_RESET") 
 				{					   
 						//if (x>(0 * canvasScale + margin) &&  x<(1024 * canvasScale + margin)) { touchButtons.continue.pressed = true; touchButtons.continue.touchId = id }
-						player1_ResetLevelPressed = true;
-						player1_ContinuePressed = true;
+						//player1_ResetLevelPressed = true;
+						//player1_ContinuePressed = true;
 						player1_ScreenTouched = true;
+						sound.playSound(SND_CHILLI);
+
 				}
 
 		}
